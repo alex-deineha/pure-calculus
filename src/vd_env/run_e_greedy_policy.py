44caa8da4e0f74288fcf422d6594ac412a2f4066
Volodymyr Donets <vol.donets@gmail.com>
2023-01-04 14:43:09 +0200

    Added version of e_greedy policy based on action rewards, and it's usage in run_e_greedy_policy.

--- src/vd_env/run_e_greedy_policy.py
+++ src/vd_env/run_e_greedy_policy.py
@@ -3,20 +3,26 @@ from tqdm import tqdm
 
 sys.path.append('../')
 from vd_env.lambda_env import get_simple_env
-from vd_env.e_greedy_policy import e_greedy_policy
+from vd_env.e_greedy_policy import e_greedy_policy, e_greedy_action_based_policy
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def run_e_greedy_policy(environment, exploration=10, max_term_reward=30, epsilon=.1, deterministic=True):
+def run_e_greedy_policy(environment, exploration=10, max_term_reward=30, epsilon=.1,
+                        deterministic=True, is_action_based=False):
     state = environment.reset()
     rewards = []
 
     while environment.is_has_next_term():
         while True:
-            action = e_greedy_policy(environment, state, explore=exploration,
-                                     count_strategies=len(environment.strategies),
-                                     epsilon=epsilon, deterministic=deterministic)
+            if is_action_based:
+                action = e_greedy_action_based_policy(environment, state,explore_steps=exploration,
+                                                      count_strategies=len(environment.strategies),
+                                                      epsilon=epsilon, deterministic=deterministic)
+            else:
+                action = e_greedy_policy(environment, state, explore=exploration,
+                                         count_strategies=len(environment.strategies),
+                                         epsilon=epsilon, deterministic=deterministic)
             state, _, done, _ = environment.step(action)
             if done:
                 break
@@ -33,22 +39,23 @@ if __name__ == '__main__':
     # seed = 0
     # random.seed(seed)
 
-    count_terms = 50
+    count_terms = 100
     max_reward = 30
     epsilon_ = .1
+    exploration = 100
 
     env = get_simple_env(count_terms=count_terms, max_step_term=max_reward)
 
     env, rewards_det = run_e_greedy_policy(env, exploration=15, max_term_reward=max_reward,
-                                           deterministic=True)
+                                           deterministic=True, is_action_based=True)
     print('Deterministic: ---------------------->')
     env.render()
     print('\n\n')
 
     env.reset_soft()
-    env, rewards_non_det = run_e_greedy_policy(env, exploration=15, max_term_reward=max_reward,
-                                               deterministic=False)
-    print('Deterministic: ---------------------->')
+    env, rewards_non_det = run_e_greedy_policy(env, exploration=exploration, max_term_reward=max_reward,
+                                               deterministic=False, is_action_based=True)
+    print('Non-Deterministic: ---------------------->')
     env.render()
     print('\n\n')
 
