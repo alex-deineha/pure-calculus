c0b11e9996a2a21ced1af35712db890f0d46e8a5
Volodymyr Donets <vol.donets@gmail.com>
2023-03-06 14:43:24 +0200

    Added correct RandomOuter and RandomInner strategies. And tested it in the notebook.

--- src/calculus_path_mod/reduction_strategy.py
+++ src/calculus_path_mod/reduction_strategy.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from calculus_path_mod.term_engine import Term
 import random
+import numpy as np
 
 
 class OneStepStrategy(ABC):
@@ -75,3 +76,68 @@ class RandomRightInnerStrategy(OneStepStrategy):
             return init_index + 1
         else:
             return self.redex_index(term._data[1], init_index + 1)
+
+
+class RandomOuterStrategy(OneStepStrategy):
+    def __init__(self, prob_norm="softmax"):
+        """:param prob_norm: str of 'softmax' or 'sum' which define calculation of probabilities"""
+        self.prob_norm = prob_norm
+
+    def _get_redexes_indexes(self, term: Term, init_index: int = 0, path_depth: int = 0):
+        if (term.kind == "atom") or (len(term.redexes) == 0):
+            raise ValueError("The term doesn't contain a redex")
+        if term.kind == "application":
+            result = dict()
+            if term.is_beta_redex:
+                result = {**result,
+                          **{init_index + 1: path_depth + 1}}
+            if len(term._data[0].redexes) != 0:
+                result = {**result,
+                          **self._get_redexes_indexes(term._data[0], init_index + 1, path_depth + 1)}
+            if len(term._data[1].redexes) != 0:
+                result = {**result,
+                          **self._get_redexes_indexes(term._data[1],
+                                                      init_index + term._data[0].vertices_number + 1,
+                                                      path_depth + 1)}
+            return result
+            # self is Abstraction:
+        return self._get_redexes_indexes(term._data[1], init_index + 1, path_depth + 1)
+
+    def redex_index(self, term: Term, init_index=0) -> int:
+        dict_redexes_indexes = self._get_redexes_indexes(term)
+        list_of_indexes = list(dict_redexes_indexes.keys())
+        list_of_prob = np.array(list(dict_redexes_indexes.values()), dtype="float64")
+
+        if self.prob_norm == "softmax":
+            max_depth = 1. + np.amax(list_of_prob)
+            list_of_prob *= -1.
+            list_of_prob += max_depth
+            list_of_prob = np.exp(list_of_prob) / np.sum(np.exp(list_of_prob))
+        elif self.prob_norm == "sum":
+            max_depth = 1. + np.amax(list_of_prob)
+            list_of_prob *= -1.
+            list_of_prob += max_depth
+            list_of_prob /= np.sum(list_of_prob)
+        else:
+            raise ValueError("Inappropriate value of prob_norm")
+
+        return np.random.choice(list_of_indexes, p=list_of_prob)
+
+
+class RandomInnerStrategy(RandomOuterStrategy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def redex_index(self, term: Term, init_index=0) -> int:
+        dict_redexes_indexes = self._get_redexes_indexes(term)
+        list_of_indexes = list(dict_redexes_indexes.keys())
+        list_of_prob = np.array(list(dict_redexes_indexes.values()), dtype="float64")
+
+        if self.prob_norm == "softmax":
+            list_of_prob = np.exp(list_of_prob) / np.sum(np.exp(list_of_prob))
+        elif self.prob_norm == "sum":
+            list_of_prob /= np.sum(list_of_prob)
+        else:
+            raise ValueError("Inappropriate value of prob_norm")
+
+        return np.random.choice(list_of_indexes, p=list_of_prob)
