9da3c79897313e4cefa17f7c74a8b991ba70b8e3
Volodymyr Donets <vol.donets@gmail.com>
2023-03-15 14:20:36 +0200

    Updated methods description in the documentation.

--- src/calculus_path_mod/reduction_strategy.py
+++ src/calculus_path_mod/reduction_strategy.py
@@ -16,6 +16,9 @@ class OneStepStrategy(ABC):
 
 class LOStrategy(OneStepStrategy):
     def redex_index(self, term: Term, init_index=0) -> int:
+        # The main idea check current vertex
+        # if it's a redex return its coordinate in the tree
+
         if (term.kind == "atom") or (len(term.redexes) == 0):
             raise ValueError("The term doesn't contain a redex")
         if term.kind == "application":
@@ -31,6 +34,10 @@ class LOStrategy(OneStepStrategy):
 
 class RIStrategy(OneStepStrategy):
     def redex_index(self, term: Term, init_index=0) -> int:
+        # The main idea go in the right vertex (object) of the application
+        # until there is no other way to go right, go the left vertex,
+        # until there is no way and define current app as a redex.
+
         if (term.kind == "atom") or (len(term.redexes) == 0):
             raise ValueError("The term doesn't contain a redex")
         if term.kind == "application":
@@ -46,6 +53,10 @@ class RIStrategy(OneStepStrategy):
 
 class RandomLeftOuterStrategy(OneStepStrategy):
     def redex_index(self, term: Term, init_index=0) -> int:
+        # The main idea check current vertex
+        # if it's a redex return its coordinate in the tree with some probability
+        # and with some probability go to the left vertex of right.
+
         count_redexes = len(term.redexes)
         if term.kind == "atom" or count_redexes == 0:
             raise ValueError("The term doesn't contain a redex")
@@ -63,6 +74,10 @@ class RandomLeftOuterStrategy(OneStepStrategy):
 
 class RandomRightInnerStrategy(OneStepStrategy):
     def redex_index(self, term: Term, init_index=0) -> int:
+        # The main idea go in the right vertex (object) of the application
+        # until there is no other way to go right, go the left vertex,
+        # until there is no way and define current app as a redex.
+
         count_redexes = len(term.redexes)
         if term.kind == "atom" or count_redexes == 0:
             raise ValueError("The term doesn't contain a redex")
@@ -80,11 +95,25 @@ class RandomRightInnerStrategy(OneStepStrategy):
 
 class RandomOuterStrategy(OneStepStrategy):
     def __init__(self, prob_norm="softmax"):
-        """:param prob_norm: str of 'softmax', or 'sum', a 'pow_val' (where val - num value of power, example 'pow_2'),
-         which define calculation of probabilities"""
+        """
+        :param prob_norm: str of 'softmax', or 'sum', a 'pow_val'
+                          (where val - num value of power, example 'pow_2'),
+                          which define calculation of probabilities
+        """
         self.prob_norm = prob_norm
 
-    def _get_redexes_indexes(self, term: Term, init_index: int = 0, path_depth: int = 0):
+    def _get_redexes_indexes(self, term: Term, init_index: int = 0, path_depth: int = 0) -> dict:
+        """
+        Using Left Outer strategy find all redexes, it's coordinates in the term tree,
+        and the depth value for each redex.
+
+        :param term: some Term is a current vertex
+        :param init_index: some index is a coordinate of the current vertex in the term tree
+        :param path_depth: depth in the term tree (simply recursion layer).
+        :return: dict where a key is a index (coordinate of redex in the term tree),
+                 and var is a depth from the tree root
+        """
+
         if (term.kind == "atom") or (len(term.redexes) == 0):
             raise ValueError("The term doesn't contain a redex")
         if term.kind == "application":
@@ -105,6 +134,9 @@ class RandomOuterStrategy(OneStepStrategy):
         return self._get_redexes_indexes(term._data[1], init_index + 1, path_depth + 1)
 
     def redex_index(self, term: Term, init_index=0) -> int:
+        # Using the redex coordinates and the depth values generate probabilities
+        # of choosing for each redex, and in the end choose and return redex coordinate.
+
         dict_redexes_indexes = self._get_redexes_indexes(term)
         list_of_indexes = list(dict_redexes_indexes.keys())
         list_of_prob = np.array(list(dict_redexes_indexes.values()), dtype="float64")
@@ -132,6 +164,9 @@ class RandomInnerStrategy(RandomOuterStrategy):
         super().__init__(*args, **kwargs)
 
     def redex_index(self, term: Term, init_index=0) -> int:
+        # Using the redex coordinates and the depth values generate probabilities
+        # of choosing for each redex, and in the end choose and return redex coordinate.
+
         dict_redexes_indexes = self._get_redexes_indexes(term)
         list_of_indexes = list(dict_redexes_indexes.keys())
         list_of_prob = np.array(list(dict_redexes_indexes.values()), dtype="float64")
