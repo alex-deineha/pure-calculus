2d49e7d1b614058557482b9e7ffe91fa41632097
Volodymyr Donets <vol.donets@gmail.com>
2023-05-30 15:54:28 +0300

    Added Z and Etta_v combinators (which are familiar to the default Y-combinator but should work with LI & RI & RO strategies)

--- src/calculus_path_mod/terms/combinators.py
+++ src/calculus_path_mod/terms/combinators.py
@@ -42,3 +42,27 @@ def y_term():
         Lambda(x, App(g_, App(x_, x_))),
         Lambda(x, App(g_, App(x_, x_)))
     ))
+
+
+def z_term():
+    """:return: Z := λf. (λx.f(λv.xxv))(λx.f(λv.xxv))"""
+
+    f, x, v = Var(), Var(), Var()
+    f_, x_, v_ = Atom(f), Atom(x), Atom(v)
+    return Lambda(f, App(
+        Lambda(x, App(f_, Lambda(v, multi_app_term(x_, x_, v_)))),
+        Lambda(x, App(f_, Lambda(v, multi_app_term(x_, x_, v_))))
+    ))
+
+
+def etta_v_term():
+    """:return: Etta_v := (λxy.y(λx.xxyz))(λxy.y(λx.xxyz))"""
+
+    x, y, z = Var(), Var(), Var()
+    x_, y_, z_ = Atom(x), Atom(y), Atom(y)
+
+    return App(
+        Lambda(x, Lambda(y, App(y_, Lambda(z, multi_app_term(x_, x_, y_, z_))))),
+        Lambda(x, Lambda(y, App(y_, Lambda(z, multi_app_term(x_, x_, y_, z_)))))
+    )
+
